ed972723b7ba8c31835b2e1ad704c43784953594
arijittest <52116553+arijittest@users.noreply.github.com>
2019-08-07 06:46:41 +0800

    Changing the design of recipe-edit page

--- recipe-app/scripts/recipes-edit.js
+++ recipe-app/scripts/recipes-edit.js
@@ -1,6 +1,8 @@
 const titleElement=document.querySelector('#recipe-title');
 const summaryElement=document.querySelector('#recipe-Summary');
-const buttonElement = document.querySelector('#add-recipe');
+const addButtonElement = document.querySelector('#add-ingredient');
+const ingredientElement = document.querySelector('#ingredient-name');
+const divIngredientElement = document.querySelector('#ingredients');
 
 const recipeId=location.hash.substring(1);
 const recipes=getRecipeArray();
@@ -24,9 +26,25 @@ summaryElement.addEventListener('input',(e)=>{
     saveRecipes(recipes);
 })
 
-buttonElement.addEventListener('click',(e)=>{
-    //recipe.title=e.target.value;
-    saveRecipes(recipes);
-    location.assign('/index.html');
-})
+// buttonElement.addEventListener('click',(e)=>{
+//     //recipe.title=e.target.value;
+//     saveRecipes(recipes);
+//     location.assign('/index.html');
+// })
  
+addButtonElement.addEventListener('click',(e)=>{
+
+    //console.log(ingredientElement.value);
+    const inputElem = document.createElement('input');
+    const spanElem = document.createElement('span');
+    const buttonElem = document.createElement('button');
+
+    inputElem.setAttribute("type","checkbox");
+    spanElem.textContent=ingredientElement.value;
+    buttonElem.textContent='x';
+
+    divIngredientElement.appendChild(inputElem);
+    divIngredientElement.appendChild(spanElem);
+    divIngredientElement.appendChild(buttonElem);
+
+})
